7b32c5d8bd16e8ae00f14efe04da820c8c9b8e58
somvitbhowmik <somvit.bhowmik@tarento.com>
2022-08-23 15:11:35 +0530

    added ceph configuration and update cloud store jar version

--- auto-creator-v2/src/main/scala/org/sunbird/job/autocreatorv2/util/CloudStorageUtil.scala
+++ auto-creator-v2/src/main/scala/org/sunbird/job/autocreatorv2/util/CloudStorageUtil.scala
@@ -13,6 +13,7 @@ class CloudStorageUtil(config: AutoCreatorV2Config) extends Serializable {
 	val cloudStorageType = config.getString("cloud_storage_type", "azure")
 	val azureStorageContainer = config.getString("azure_storage_container", "")
 	val awsStorageContainer = config.getString("aws_storage_container", "")
+	val cephs3StorageContainer = config.getString("cephs3_storage_container", "")
 
 	@throws[Exception]
 	def getService: BaseStorageService = {
@@ -25,6 +26,11 @@ class CloudStorageUtil(config: AutoCreatorV2Config) extends Serializable {
 				val awsStorageKey = config.getString("aws_storage_key", "")
 				val awsStorageSecret = config.getString("aws_storage_secret", "")
 				storageService = StorageServiceFactory.getStorageService(StorageConfig(cloudStorageType, awsStorageKey, awsStorageSecret))
+			} else if (StringUtils.equalsIgnoreCase(cloudStorageType, "cephs3")) {
+				val storageKey = config.getString("cephs3_storage_key", "");
+				val storageSecret = config.getString("cephs3_storage_secret", "");
+				val endPoint = config.getString("cephs3_storage_endpoint", "");
+				storageService = StorageServiceFactory.getStorageService(StorageConfig(cloudStorageType, storageKey, storageSecret, Option(endPoint)))
 			} else throw new Exception("Error while initialising cloud storage: " + cloudStorageType)
 		}
 		storageService
@@ -34,6 +40,7 @@ class CloudStorageUtil(config: AutoCreatorV2Config) extends Serializable {
 		cloudStorageType match {
 			case "azure" => azureStorageContainer
 			case "aws" => awsStorageContainer
+			case "cephs3" => cephs3StorageContainer
 			case _ => throw new Exception("Container name not configured.")
 		}
 	}
