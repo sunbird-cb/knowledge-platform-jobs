8e7fc618a09dbb3939134eaf3974bc7aefb7b58c
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2023-09-25 09:46:46 +0530

    4.8.6.1 post publish flink (#27)
    * Initial Commit
    
    1. postPublicRelationUpdateStream Added Kafka consumer and producer.
    2. Added the configuration in the PostPublishProcessorConfig.
    3. Added service method postPublishRelationUpdate to the BatchCreation.
    4. Added a function classs PostPublishRelationUpdaterFunction.
    5. Added the configuration in the Post-publish-processor.conf
    
    * Code changes
    
    Code changes
    
    * Dev #104434-Enhance post publish processor flink job and add parentCollections details against Course.
    
    Code Review fixes.
    
    * Added method to get course from redis
    
    * Dev #104434- Enhance post publish processor flink job and add parentCollections details against Course.
    
    Code Fixes
    
    * Dev #104434-Enhance post publish processor flink job and add parentCollections details against Course
    
    Content State Url Updated.
    
    * Updated code. Using proper system update API
    
    * Optimized code
    
    * Formatted code
    
    * Formatted code
    
    * reading keyspace from config
    
    * reading keyspace from config
    
    ---------
    
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>

---
+++ post-publish-processor/src/main/scala/org/sunbird/job/postpublish/functions/PostPublishRelationUpdaterFunction.scala
@@ -0,0 +1,191 @@
+package org.sunbird.job.postpublish.functions
+
+import com.datastax.driver.core.querybuilder.QueryBuilder
+import org.apache.commons.collections.CollectionUtils
+import org.apache.commons.lang3.StringUtils
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.http.client.methods.HttpPatch
+import org.apache.http.entity.{ContentType, StringEntity}
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.{HttpResponse, StatusLine}
+import org.slf4j.LoggerFactory
+import org.sunbird.job.cache.{DataCache, RedisConnect}
+import org.sunbird.job.exception.APIException
+import org.sunbird.job.postpublish.helpers.PostPublishRelationUpdater
+import org.sunbird.job.postpublish.task.PostPublishProcessorConfig
+import org.sunbird.job.util.{CassandraUtil, HTTPResponse, HttpUtil, JSONUtil}
+import org.sunbird.job.{BaseProcessFunction, Metrics}
+
+import java.time.format.DateTimeFormatter
+import java.time.{ZoneId, ZonedDateTime}
+import java.util
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
+
+/** @author
+  *   mahesh.vakkund
+  */
+class PostPublishRelationUpdaterFunction(
+    config: PostPublishProcessorConfig,
+    httpUtil: HttpUtil,
+    @transient var cassandraUtil: CassandraUtil = null
+) extends BaseProcessFunction[java.util.Map[String, AnyRef], String](config)
+    with PostPublishRelationUpdater {
+
+  private[this] val logger =
+    LoggerFactory.getLogger(classOf[PostPublishRelationUpdaterFunction])
+  lazy private val mapper: ObjectMapper = new ObjectMapper()
+  private var cache: DataCache = _
+
+  override def open(parameters: Configuration): Unit = {
+    super.open(parameters)
+    cassandraUtil = new CassandraUtil(config.dbHost, config.dbPort)
+    val redisConnect = new RedisConnect(config)
+    cache =
+      new DataCache(config, redisConnect, config.contentCacheStore, List())
+    cache.init()
+  }
+
+  override def close(): Unit = {
+    cassandraUtil.close()
+    cache.close()
+    super.close()
+  }
+
+  private def postPublishRelationUpdate(
+      eData: util.Map[String, AnyRef]
+  )(implicit
+      config: PostPublishProcessorConfig,
+      httpUtil: HttpUtil,
+      cassandraUtil: CassandraUtil,
+      metrics: Metrics
+  ): Unit = {
+    val programHierarchy = getProgramHierarchy(
+      eData.get("identifier").asInstanceOf[String]
+    )(metrics, config, cache, httpUtil)
+    if (programHierarchy.isEmpty) {
+      logger.info(
+        "PostPublishRelationUpdaterFunction :: Failed to get program Hierarchy."
+      )
+      return
+    }
+
+    val childCourseList = programHierarchy
+      .get(config.children)
+      .asInstanceOf[List[Map[String, AnyRef]]]
+    for (childNode <- childCourseList) {
+      val primaryCategory: String =
+        childNode.get(config.primaryCategory).asInstanceOf[String]
+      if (primaryCategory.equalsIgnoreCase("Course")) {
+        val contentObj: java.util.Map[String, AnyRef] = getCourseInfo(
+          childNode.get("identifier").asInstanceOf[String]
+        )(metrics, config, cache, httpUtil)
+        val parentCollections: ListBuffer[String] = contentObj
+          .getOrDefault(config.parentCollections, ListBuffer.empty[String])
+          .asInstanceOf[ListBuffer[String]]
+        val versionKey: String =
+          contentObj.get(config.versionKey).asInstanceOf[String]
+        val identifier: String =
+          childNode.get("identifier").asInstanceOf[String]
+        if (parentCollections.isEmpty) {
+          parentCollections += identifier
+        } else {
+          if (!parentCollections.contains(identifier)) {
+            parentCollections += identifier
+          }
+        }
+        val requestData: Map[String, Any] = Map(
+          "request" -> Map(
+            "content" -> Map(
+              "versionKey" -> versionKey,
+              "parentCollections" -> parentCollections.toList
+            )
+          )
+        )
+        val jsonString: String = JSONUtil.serialize(requestData)
+        val patchRequest = new HttpPatch(
+          config.contentSystemUpdatePath + identifier
+        )
+        patchRequest.setEntity(
+          new StringEntity(jsonString, ContentType.APPLICATION_JSON)
+        )
+        val httpClient = HttpClients.createDefault()
+        val response: HttpResponse = httpClient.execute(patchRequest)
+        val statusLine: StatusLine = response.getStatusLine
+        val statusCode: Int = statusLine.getStatusCode
+        if (statusCode == 200) {
+          logger.info("Processed the request.")
+        } else {
+          logger.error(
+            "Received error response for system update API. Response: " + JSONUtil
+              .serialize(response)
+          )
+        }
+      }
+    }
+  }
+
+  def getProgramHierarchy(programId: String)(
+      metrics: Metrics,
+      config: PostPublishProcessorConfig,
+      cache: DataCache,
+      httpUtil: HttpUtil
+  ): java.util.Map[String, AnyRef] = {
+    val query = QueryBuilder
+      .select(config.Hierarchy)
+      .from(config.hierarchyStoreKeySpace, config.contentHierarchyTable)
+      .where(QueryBuilder.eq(config.identifier, programId))
+    val row = cassandraUtil.find(query.toString)
+    if (CollectionUtils.isNotEmpty(row)) {
+      val hierarchy =
+        row.asScala.head.getObject(config.Hierarchy).asInstanceOf[String]
+      if (StringUtils.isNotBlank(hierarchy))
+        mapper.readValue(hierarchy, classOf[java.util.Map[String, AnyRef]])
+      else new java.util.HashMap[String, AnyRef]()
+    } else new java.util.HashMap[String, AnyRef]()
+  }
+
+  override def processElement(
+      eData: java.util.Map[String, AnyRef],
+      context: ProcessFunction[java.util.Map[String, AnyRef], String]#Context,
+      metrics: Metrics
+  ): Unit = {
+    val identifier = eData.getOrDefault("identifier", "").asInstanceOf[String]
+    val isValidProgram: Boolean =
+      verifyPrimaryCategory(identifier)(metrics, config, httpUtil, cache)
+    if (!isValidProgram) {
+      metrics.incCounter(config.postPublishRelationUpdateEventCount)
+      logger.info(
+        "PostPublishRelationUpdaterFunction:: started for Content : " + identifier
+      )
+      try {
+        postPublishRelationUpdate(eData)(
+          config,
+          httpUtil,
+          cassandraUtil,
+          metrics
+        )
+        metrics.incCounter(config.postPublishRelationUpdateSuccessCount)
+        logger.info(
+          "PostPublishRelationUpdaterFunction:: Completed for ContentId : " + identifier
+        )
+      } catch {
+        case ex: Throwable =>
+          logger.error(
+            s"Error while processing message for identifier : ${identifier}.",
+            ex
+          )
+          metrics.incCounter(config.postPublishRelationUpdateFailureCount)
+          throw ex
+      }
+    } else {
+      logger.info(
+        "PostPublishRelationUpdaterFunction:: Nothing to do for ContentId : " + identifier
+      )
+    }
+  }
+
+  override def metricsList(): List[String] = ???
+}
---
+++ post-publish-processor/src/main/scala/org/sunbird/job/postpublish/helpers/PostPublishRelationUpdater.scala
@@ -0,0 +1,130 @@
+package org.sunbird.job.postpublish.helpers
+
+import org.apache.flink.configuration.Configuration
+import org.slf4j.LoggerFactory
+import org.sunbird.job.Metrics
+import org.sunbird.job.cache.{DataCache, RedisConnect}
+import org.sunbird.job.exception.APIException
+import org.sunbird.job.postpublish.domain.Event
+import org.sunbird.job.postpublish.task.PostPublishProcessorConfig
+import org.sunbird.job.util._
+
+/** @author
+  *   mahesh.vakkund
+  */
+trait PostPublishRelationUpdater {
+
+  private[this] val logger =
+    LoggerFactory.getLogger(classOf[PostPublishRelationUpdater])
+
+  def verifyPrimaryCategory(identifier: String)(
+      metrics: Metrics,
+      config: PostPublishProcessorConfig,
+      httpUtil: HttpUtil,
+      cache: DataCache
+  ): Boolean = {
+    logger.info(
+      "Verify Program post-publish required for content: " + identifier
+    )
+    // Get the primary Categories for the courses here
+    var isValidProgram = false
+    val contentObj: java.util.Map[String, AnyRef] =
+      getCourseInfo(identifier)(metrics, config, cache, httpUtil)
+    if (contentObj.isEmpty) {
+      contentObj.get("primaryCategory") match {
+        case Some("Program" | "Curated Program" | "Blended Program") =>
+          isValidProgram = true
+      }
+    }
+    isValidProgram
+  }
+
+  def getCourseInfo(courseId: String)(
+      metrics: Metrics,
+      config: PostPublishProcessorConfig,
+      cache: DataCache,
+      httpUtil: HttpUtil
+  ): java.util.Map[String, AnyRef] = {
+    val courseMetadata = cache.getWithRetry(courseId)
+    if (null == courseMetadata || courseMetadata.isEmpty) {
+      val url =
+        config.contentReadURL + "/" + courseId + "?fields=identifier,name,versionKey,parentCollections,primaryCategory"
+      val response = getAPICall(url, "content")(config, httpUtil, metrics)
+      val courseName = StringContext
+        .processEscapes(
+          response.getOrElse(config.name, "").asInstanceOf[String]
+        )
+        .filter(_ >= ' ')
+      val primaryCategory = StringContext
+        .processEscapes(
+          response.getOrElse(config.primaryCategory, "").asInstanceOf[String]
+        )
+        .filter(_ >= ' ')
+      val parentCollections = response
+        .getOrElse("parentCollections", List.empty[String])
+        .asInstanceOf[List[String]]
+      val courseInfoMap: java.util.Map[String, AnyRef] =
+        new java.util.HashMap[String, AnyRef]()
+      courseInfoMap.put("courseId", courseId)
+      courseInfoMap.put("courseName", courseName)
+      courseInfoMap.put("parentCollections", parentCollections)
+      courseInfoMap.put("primaryCategory", primaryCategory)
+      courseInfoMap
+    } else {
+      val courseName = StringContext
+        .processEscapes(
+          courseMetadata.getOrElse(config.name, "").asInstanceOf[String]
+        )
+        .filter(_ >= ' ')
+      val primaryCategory = StringContext
+        .processEscapes(
+          courseMetadata
+            .getOrElse(config.primaryCategory, "")
+            .asInstanceOf[String]
+        )
+        .filter(_ >= ' ')
+      val parentCollections = courseMetadata
+        .getOrElse("parentCollections", List.empty[String])
+        .asInstanceOf[List[String]]
+      val courseInfoMap: java.util.Map[String, AnyRef] =
+        new java.util.HashMap[String, AnyRef]()
+      courseInfoMap.put("courseId", courseId)
+      courseInfoMap.put("courseName", courseName)
+      courseInfoMap.put("parentCollections", parentCollections)
+      courseInfoMap.put("primaryCategory", primaryCategory)
+      courseInfoMap
+    }
+
+  }
+
+  def getAPICall(url: String, responseParam: String)(
+      config: PostPublishProcessorConfig,
+      httpUtil: HttpUtil,
+      metrics: Metrics
+  ): Map[String, AnyRef] = {
+    val response = httpUtil.get(url, config.defaultHeaders)
+    if (200 == response.status) {
+      ScalaJsonUtil
+        .deserialize[Map[String, AnyRef]](response.body)
+        .getOrElse("result", Map[String, AnyRef]())
+        .asInstanceOf[Map[String, AnyRef]]
+        .getOrElse(responseParam, Map[String, AnyRef]())
+        .asInstanceOf[Map[String, AnyRef]]
+    } else if (
+      400 == response.status && response.body.contains(
+        config.userAccBlockedErrCode
+      )
+    ) {
+      metrics.incCounter(config.skippedEventCount)
+      logger.error(
+        s"Error while fetching user details for ${url}: " + response.status + " :: " + response.body
+      )
+      Map[String, AnyRef]()
+    } else {
+      throw new Exception(
+        s"Error from get API : ${url}, with response: ${response}"
+      )
+    }
+  }
+
+}
