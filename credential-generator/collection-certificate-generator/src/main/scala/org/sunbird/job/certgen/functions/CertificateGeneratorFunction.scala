3a43796d710ec054ae8addd84132937815ca8df2
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-10-24 11:45:52 +0530

    Disabled user feed notification

--- credential-generator/collection-certificate-generator/src/main/scala/org/sunbird/job/certgen/functions/CertificateGeneratorFunction.scala
+++ credential-generator/collection-certificate-generator/src/main/scala/org/sunbird/job/certgen/functions/CertificateGeneratorFunction.scala
@@ -309,7 +309,7 @@ class CertificateGeneratorFunction(config: CertificateGeneratorConfig, httpUtil:
           logger.info("pushAuditEvent: certificate audit event success {}", audit)
           context.output(config.notifierOutputTag, NotificationMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId,
             certMetaData.batchId, certMetaData.templateId, event.partition, event.offset, event.providerName, event.coursePosterImage))
-          context.output(config.userFeedOutputTag, UserFeedMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId, event.partition, event.offset))
+          //context.output(config.userFeedOutputTag, UserFeedMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId, event.partition, event.offset))
         } else {
           metrics.incCounter(config.failedEventCount)
           throw new Exception(s"Update certificates to enrolments failed: ${event}")
