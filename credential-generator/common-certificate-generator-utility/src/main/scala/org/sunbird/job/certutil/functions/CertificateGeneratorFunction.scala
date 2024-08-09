67fe4aab9f536c7b8570934d6228372eb09098e3
saipradeep_ravipati <saipradeep.ravipati@tarento.com>
2024-08-09 11:59:05 +0530

    remove un-wanted commits

--- credential-generator/common-certificate-generator-utility/src/main/scala/org/sunbird/job/certutil/functions/CertificateGeneratorFunction.scala
+++ credential-generator/common-certificate-generator-utility/src/main/scala/org/sunbird/job/certutil/functions/CertificateGeneratorFunction.scala
@@ -64,7 +64,6 @@ class CertificateGeneratorFunction(config: CertificateGeneratorConfig, httpUtil:
   override def processElement(event: Event,
                               context: KeyedProcessFunction[String, Event, String]#Context,
                               metrics: Metrics): Unit = {
-    println("I am ****** NEW Common Collection Certificate Generator ***SAI-NEW***** Certificate data: " + event)
     metrics.incCounter(config.totalEventsCount)
     try {
       val certValidator = new CertValidator()
